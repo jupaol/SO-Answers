897582e89123c148e9c4db1f49c96e554e07302f
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-09-24 18:34:15 -0500

    Testing satellite assemblies

---
+++ Visual Studio 2012/src/SO/WebForms/WebForms_1/Topics/Globalization/ReadingResourcesFromSatelliteAssemblies.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReadingResourcesFromSatelliteAssemblies.aspx.cs" Inherits="WebForms_1.Topics.Globalization.ReadingResourcesFromSatelliteAssemblies" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label Text="text" runat="server" ID="msg" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Visual Studio 2012/src/SO/WebForms/WebForms_1/Topics/Globalization/ReadingResourcesFromSatelliteAssemblies.aspx.cs
@@ -0,0 +1,27 @@
+﻿using SampleControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms_1.Topics.Globalization
+{
+    public partial class ReadingResourcesFromSatelliteAssemblies : System.Web.UI.Page
+    {
+        protected override void InitializeCulture()
+        {
+            this.Culture = "it-IT";
+            this.UICulture = "it-IT";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var rm = new ResourceManager("SampleControl.VerificationResources", typeof(ClientVerification).Assembly);
+
+            this.msg.Text = rm.GetString("Correct");
+        }
+    }
+}
